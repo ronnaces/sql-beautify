331a947691d54ebcc4643741ea92acec914b0969
kunlong-luo <luokunlong.loong@gmail.com>
2025-08-22 15:15:33 +0800

    refactor: 精简 Java 示例代码
    - 将冗长的 Java DO 类示例替换为简单的 "public class" 关键字
    - 保持代码整洁并减少不必要的示例代码长度

--- main.py
+++ main.py
@@ -613,57 +613,7 @@ with tab2:
     st.subheader("☕ Java Domain Object to SQL")
     st.markdown("Paste your Java DO class below to generate CREATE TABLE statement")
 
-    java_example = """package cn.iocoder.yudao.module.bpm.dal.dataobject.definition;
-
-import cn.iocoder.yudao.framework.mybatis.core.dataobject.BaseDO;
-import com.baomidou.mybatisplus.annotation.KeySequence;
-import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableName;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-/**
- * BPM 流程分类 DO
- *
- * @author 芋道源码
- */
-@TableName("bpm_category")
-@KeySequence("bpm_category_seq")
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-public class BpmCategoryDO extends BaseDO {
-    /**
-     * 分类编号
-     */
-    @TableId
-    private Long id;
-    /**
-     * 分类名
-     */
-    private String name;
-    /**
-     * 分类标志
-     */
-    private String code;
-    /**
-     * 分类描述
-     */
-    private String description;
-    /**
-     * 分类状态
-     *
-     * 枚举 {@link cn.iocoder.yudao.framework.common.enums.CommonStatusEnum}
-     */
-    private Integer status;
-    /**
-     * 分类排序
-     */
-    private Integer sort;
-}"""
+    java_example = """public class"""
 
     java_code = st.text_area(
         "Java DO Class Code",
